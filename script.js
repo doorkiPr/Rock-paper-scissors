070f5c906531a71e9d64e649a78401144b901933
doorkiPr <boumarafchakib7@gmail.com>
2023-12-09 06:24:27 +0100

    ADD:add function to return an answer based on the number gotten from getRandomNumber function

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
+
+function getComputerChoice() {
+    let randomNumber = getRandomNumber(1, 3);
+
+    return randomNumber === 1 ? "rock"
+        : randomNumber === 2 ? "scissors"
+            : "paper";
+
+}
