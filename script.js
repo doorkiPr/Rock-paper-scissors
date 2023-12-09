546259fef176ada9b518771bd4fe591b36438da5
doorkiPr <boumarafchakib7@gmail.com>
2023-12-09 07:17:24 +0100

    ADD:function that takes and compares answers , returns different strings for each outcome

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -10,3 +11,54 @@ function getComputerChoice() {
             : "paper";
 
 }
+
+function playRound(playerSelection, computerSelection) {
+
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection();
+
+    if (playerChoice === "rock" && computerChoice === "paper") {
+        return "You Loose paper  beats rock !"
+    }
+    else if (playerChoice === "rock" && computerChoice === "scissors") {
+        return "You Win rock beats scissors ! "
+    }
+
+    else if (playerChoice === "paper" && computerChoice === "scissors") {
+        return "You Loose scissors beats paper !"
+    }
+
+    else if (playerChoice === "paper" && computerChoice === "rock") {
+        return "You Win paper beats rock ! "
+    }
+
+    else if (playerChoice === "scissors" && computerChoice === "rock") {
+        return "You Loose rock beats scissors! "
+    }
+
+    else if (playerChoice === "scissors" && computerChoice === "paper") {
+        return "You Win scissors beats paper !"
+    }
+
+    else if (playerChoice === computerChoice) {
+        let newPlayerChoice = prompt("It's a tie ! pick a new answer");  
+
+        return playRound(newPlayerChoice, getComputerChoice)
+
+    }
+    else if (playerChoice != "rock" || playerChoice != "scissors" || playerChoice != "paper") {
+
+        let newPlayerChoice = prompt("only rock scissors and paper allowed ! pick a new answer");
+
+        return playRound(newPlayerChoice, getComputerChoice)
+    }
+
+}
+
+
+
+
+// take player and computer choices 
+// compare the choices to declare a winner 
+// if it finds a tie replay the round 
+// if u missinput it replays the round
